8bd82e45e277e2b78563d27312a0bea4cf4f46c2
Nicholas Chung-Hun <nicholas61>
2020-10-23 16:24:30 -0400

    temporary

---
+++ backend/controllers/getRealTimeData.go
@@ -0,0 +1,13 @@
+
+type QuestradeAccount struct {
+	
+}
+
+func (c *Client) QuestradeRequest(method string, url string, body interface{}) (*Response, error) {
+	requestBody, err := json.Marshal(map[string]string{
+
+	})
+
+
+
+}
